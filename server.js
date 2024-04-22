8d9f6da961711b2a677f90fa7f967cbdd1add974
satyakamtyagi369 <satyakamtyagi8979@gmail.com>
2024-04-22 18:14:43 +0530

    add process env PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-
+const port= process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -140,6 +140,6 @@ app.post('/posts/:postId/delete', (req, res) => {
      });
  });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server started on port 3000');
 });
